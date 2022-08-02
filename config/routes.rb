4b7e21b95dcbd1b0ff5528b88fa0dbe3513878bb
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-08-02 12:40:33 +0300

    Transactions and categories created

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  resources :transactions
+  resources :categories
+  root "users#index"
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root to: "users#index"
+  
 end
